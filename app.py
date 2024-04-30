71f91c867ae78adb01bd553965766ab942618ba7
Phani <113979652+phani040@users.noreply.github.com>
2024-04-30 23:22:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!'
+    return 'Hello, This is simple python Flask App for CI/CD pipeline'
 
 if __name__ == '__main__':
     app.run()
